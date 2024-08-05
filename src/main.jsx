d918bbccb6e87ce9143b3e1bf80d7dfdb27778f5
GannaKov <zlatta2000@gmail.com>
2024-08-05 12:55:39 +0200

    basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,30 +9,33 @@ import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import Contact from "./routes/Contact/Contact";
 import { getContactById, getContacts } from "./services/requests";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: async () => {
-      const contacts = await getContacts();
-      return { contacts };
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      loader: async () => {
+        const contacts = await getContacts();
+        return { contacts };
+      },
     },
-  },
-  {
-    path: "/destroy/:contactId",
-    action: destroyAction,
-  },
-  {
-    path: "contact/:contactId",
-    element: <Contact />,
-    errorElement: <ErrorPage />,
-    loader: async ({ params }) => {
-      const result = await getContactById(params.contactId);
-      return { result };
+    {
+      path: "/destroy/:contactId",
+      action: destroyAction,
     },
-  },
-]);
+    {
+      path: "contact/:contactId",
+      element: <Contact />,
+      errorElement: <ErrorPage />,
+      loader: async ({ params }) => {
+        const result = await getContactById(params.contactId);
+        return { result };
+      },
+    },
+  ],
+  { basename: "/test-task-n/" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
